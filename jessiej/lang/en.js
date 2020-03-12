514e3414c6b88ac2dd453942b5a9d45138e96de6
Computerdude77 <55714746+Computerdude77@users.noreply.github.com>
2020-03-11 23:41:27 -0500

    Added Jessie J page for testing

---
+++ jessiej/lang/en.js
@@ -0,0 +1,78 @@
+/* =============================================================== *\
+
+	ANGLAIS
+
+\* =============================================================== */
+
+var translateObj =  {
+	code:'EN',
+	name:'English',
+	trad:{
+		bt:{
+			week:'Week',
+			month:'Month',
+			all:'All',
+			yes:'Yes',
+			ok:'Ok',
+			sure:'Sure',
+			no:'No',
+			nope:'Nope',
+			later:'Later',
+			update:'Update',
+			retry:'Retry',
+			replay:'Replay',
+			create:'Create',
+			share:'Share',
+			save:'Save',
+			info:'Info',
+			cancel:'Cancel',
+			more:'More',
+			understand:'I understand'
+		},
+		txt:{
+			/* http://library.princeton.edu/departments/tsd/katmandu/reference/months.html */
+			m1:'Jan.', m2:'Feb.', m3:'Mar', m4:'Apr.', m5:'May', m6:'June', m7:'July.', m8:'Aug', m9:'Sept.', m10:'Oct.', m11:'Nov.', m12:'Dec.',
+			replayTitle:"Replay mode",
+			replayText:"Happy with your beat? Save it and share it!",
+			randomTitle:"Random mode",
+			randomText:"Let the beat play itself, pump up the volume and enjoy!",
+			createdBy:'Created by',
+			dedicatedTo:'Dedicated to',
+			cityState:'City, State',
+			email:'Email address*',
+			inputName:"First name and last initial*",
+			inputTitle:"Beat title*",
+		},
+		pop:{
+			owner:{ 	title : "Site owner",
+						text  : "Mars Chocolate North America<br><br><div class='info'>800 High Street<br>Hackettstown, NJ 07840 USA<br>Phone Number: 1-800-551-0702</div>"
+					},
+			sorry:{ 	title : "Hey buddy, we're sorry",
+						text  : "Our Marketing Code states that you are not eligible to view the content that you have requested. Unfortunately, this is as far as you can go on this section of our website."
+					},
+						
+			dragDropTitle:"Drag 'n drop",
+			dragDropText:"Drag and drop an icon on<br>a character to start recording",
+			toShortTitle:"Too short",
+			toShortText:"To save your beat you have to record<br>at least 30 seconds",
+			bonusPlayingTitle:"Bonus playing",
+			bonusPlayingText:"You can't start recording<br>while bonus is playing",
+			recOkTitle:"Beat finished",
+			replayOkTitle:"Create your own beat",
+			replayOkText:"Do you want to create your own beat?",
+			formTitle:"Save your beat",
+			shareTitle:"Beat saved",
+			shareText:"Thanks for submitting. Your beat could<br>become an M&M’S® TV commercial!<br>Share it with <span class='txt-red'>#BitesizeBeat</span>.",
+			pauseTitle:"Game paused",
+			pauseText:"Hey dude, where did you go?<br>Ready to beat again?",
+	  		notFoundTitle:"Beat not found",
+	  		notFoundText:"Oops! We cannot access your beat for now.",
+	  		sharingBugTitle:"Sorry buddy",
+	  		sharingBugText:"We lost connection and can't save<br>your beat right now. Please try again.",
+	  		shareMessage:"M&M’S® & Incredibox present Bite-size Beats",
+	  		sharePresentMix:"Check out my #BitesizeBeat, it could end up on TV! Thanks @mmschocolate & @incredibox_",
+	  		shareClipboard:"The link of your beat has been copied to the clipboard!",
+	  		infoTitle:"Info"
+		}
+	}
+};
